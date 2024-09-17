ad2648360f246b98aef502eb439fe034afc20345
Alex Cristici <alex.cristici@gmail.com>
2024-09-17 14:29:33 +0300

    Added OpenGL code for symbol ubo consolidation.

--- src/mbgl/renderer/layers/symbol_layer_tweaker.cpp
+++ src/mbgl/renderer/layers/symbol_layer_tweaker.cpp
@@ -99,29 +99,10 @@ void SymbolLayerTweaker::execute(LayerGroupBase& layerGroup, const PaintParamete
     auto& layerUniforms = layerGroup.mutableUniformBuffers();
     layerUniforms.set(idSymbolEvaluatedPropsUBO, evaluatedPropsUniformBuffer);
 
-#if MLN_RENDER_BACKEND_METAL
     int i = 0;
     std::vector<SymbolDrawableUBO> drawableUBOVector(layerGroup.getDrawableCount());
     std::vector<SymbolTilePropsUBO> tilePropsUBOVector(layerGroup.getDrawableCount());
     std::vector<SymbolInterpolateUBO> interpolateUBOVector(layerGroup.getDrawableCount());
-#else
-    const auto getInterpUBO =
-        [&](const UnwrappedTileID& tileID, bool isText, const SymbolBucket::PaintProperties& paintProps) {
-            auto result = interpUBOs.insert(
-                std::make_pair(InterpUBOKey{tileID, isText}, InterpUBOValue{{}, parameters.frameCount}));
-            if (result.second) {
-                // new item inserted
-                const auto interpolateBuf = buildInterpUBO(paintProps, isText, zoom);
-                result.first->second.ubo = context.createUniformBuffer(&interpolateBuf, sizeof(interpolateBuf));
-            } else if (result.first->second.updatedFrame < parameters.frameCount) {
-                // existing item found, but hasn't been updated this frame
-                const auto interpolateBuf = buildInterpUBO(paintProps, isText, zoom);
-                result.first->second.ubo->update(&interpolateBuf, sizeof(interpolateBuf));
-                result.first->second.updatedFrame = parameters.frameCount;
-            }
-            return result.first->second.ubo;
-        };
-#endif
 
     const auto camDist = state.getCameraToCenterDistance();
     visitLayerGroupDrawables(layerGroup, [&](gfx::Drawable& drawable) {
@@ -178,11 +159,7 @@ void SymbolLayerTweaker::execute(LayerGroupBase& layerGroup, const PaintParamete
         // Unpitched point labels need to have their rotation applied after projection
         const bool rotateInShader = rotateWithMap && !pitchWithMap && !alongLine;
 
-#if MLN_RENDER_BACKEND_METAL
         drawableUBOVector[i] = {
-#else
-        const SymbolDrawableUBO drawableUBO = {
-#endif
             /*.matrix=*/util::cast<float>(matrix),
             /*.label_plane_matrix=*/util::cast<float>(labelPlaneMatrix),
             /*.coord_matrix=*/util::cast<float>(glCoordMatrix),
@@ -197,36 +174,27 @@ void SymbolLayerTweaker::execute(LayerGroupBase& layerGroup, const PaintParamete
 
         const auto& sizeBinder = isText ? bucket->textSizeBinder : bucket->iconSizeBinder;
         const auto size = sizeBinder->evaluateForZoom(currentZoom);
-
-#if MLN_RENDER_BACKEND_METAL
-        tilePropsUBOVector[i] = SymbolTilePropsUBO {
-#else
-        const auto tileUBO = SymbolTilePropsUBO{
-#endif
+        tilePropsUBOVector[i] = SymbolTilePropsUBO{
             /* .is_text = */ isText,
-                /* .is_halo = */ symbolData.isHalo,
-                /* .pitch_with_map = */ (symbolData.pitchAlignment == style::AlignmentType::Map),
-                /* .is_size_zoom_constant = */ size.isZoomConstant,
-                /* .is_size_feature_constant = */ size.isFeatureConstant,
-                /* .size_t = */ size.sizeT,
-                /* .size = */ size.size,
-                /* .padding = */ 0,
+            /* .is_halo = */ symbolData.isHalo,
+            /* .pitch_with_map = */ (symbolData.pitchAlignment == style::AlignmentType::Map),
+            /* .is_size_zoom_constant = */ size.isZoomConstant,
+            /* .is_size_feature_constant = */ size.isFeatureConstant,
+            /* .size_t = */ size.sizeT,
+            /* .size = */ size.size,
+            /* .padding = */ 0,
         };
 
-#if MLN_RENDER_BACKEND_METAL
         interpolateUBOVector[i] = buildInterpUBO(paintProperties, isText, zoom);
 
         drawable.setUBOIndex(i);
         i++;
-#else
-        auto& drawableUniforms = drawable.mutableUniformBuffers();
-        drawableUniforms.createOrUpdate(idSymbolDrawableUBO, &drawableUBO, context);
-        drawableUniforms.createOrUpdate(idSymbolTilePropsUBO, &tileUBO, context);
-        drawableUniforms.set(idSymbolInterpolateUBO, getInterpUBO(tileID, isText, paintProperties));
-#endif
     });
 
-#if MLN_RENDER_BACKEND_METAL
+    if (layerGroup.getDrawableCount() > 60) {
+        assert(false);
+    }
+
     const size_t drawableUBOVectorSize = sizeof(SymbolDrawableUBO) * drawableUBOVector.size();
     if (!drawableBuffer || drawableBuffer->getSize() < drawableUBOVectorSize) {
         drawableBuffer = context.createUniformBuffer(drawableUBOVector.data(), drawableUBOVectorSize);
@@ -251,19 +219,6 @@ void SymbolLayerTweaker::execute(LayerGroupBase& layerGroup, const PaintParamete
     layerUniforms.set(idSymbolDrawableUBO, drawableBuffer);
     layerUniforms.set(idSymbolTilePropsUBO, tilePropsBuffer);
     layerUniforms.set(idSymbolInterpolateUBO, interpolateBuffer);
-#else
-    // Regularly remove UBOs which are not being updated
-    constexpr int pruneFrameInterval = 10;
-    if ((parameters.frameCount % pruneFrameInterval) == 0) {
-        for (auto i = interpUBOs.begin(); i != interpUBOs.end();) {
-            if (i->second.updatedFrame < parameters.frameCount) {
-                i = interpUBOs.erase(i);
-            } else {
-                i++;
-            }
-        }
-    }
-#endif
 }
 
 } // namespace mbgl
