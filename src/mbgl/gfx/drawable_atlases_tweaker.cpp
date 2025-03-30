de53a775a5a456d1b9896a12ba95b6be4e65f309
Alex Cristici <alex.cristici@gmail.com>
2025-03-31 00:41:42 +0300

    Removed unused code.

--- src/mbgl/gfx/drawable_atlases_tweaker.cpp
+++ src/mbgl/gfx/drawable_atlases_tweaker.cpp
@@ -1,7 +1,6 @@
 #include <mbgl/gfx/drawable_atlases_tweaker.hpp>
 
 #include <mbgl/gfx/drawable.hpp>
-#include <mbgl/gfx/dynamic_texture.hpp>
 #include <mbgl/renderer/tile_render_data.hpp>
 #include <mbgl/shaders/shader_program_base.hpp>
 #include <mbgl/renderer/paint_parameters.hpp>
@@ -9,7 +8,7 @@
 namespace mbgl {
 namespace gfx {
 
-void DrawableAtlasesTweaker::setupTextures(gfx::Drawable& drawable, [[maybe_unused]] const bool linearFilterForIcons) {
+void DrawableAtlasesTweaker::setupTextures(gfx::Drawable& drawable, const bool linearFilterForIcons) {
     if (const auto& shader = drawable.getShader()) {
         if (glyphTextureId && atlases) {
             if (atlases->glyph) {
