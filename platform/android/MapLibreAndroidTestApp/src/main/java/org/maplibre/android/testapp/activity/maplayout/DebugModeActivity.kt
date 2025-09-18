07e5c26e798bcc10bd96ee62f60d2719e3d857f5
Alex Cristici <alex.cristici@gmail.com>
2025-09-19 02:25:01 +0300

    Example of rendering metric.

--- platform/android/MapLibreAndroidTestApp/src/main/java/org/maplibre/android/testapp/activity/maplayout/DebugModeActivity.kt
+++ platform/android/MapLibreAndroidTestApp/src/main/java/org/maplibre/android/testapp/activity/maplayout/DebugModeActivity.kt
@@ -8,6 +8,7 @@ import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import org.maplibre.android.log.Logger
 import org.maplibre.android.maps.*
 import org.maplibre.android.maps.MapLibreMap.OnCameraMoveListener
 import org.maplibre.android.maps.MapLibreMap.OnFpsChangedListener
@@ -16,9 +17,11 @@ import org.maplibre.android.style.layers.Layer
 import org.maplibre.android.style.layers.Property
 import org.maplibre.android.style.layers.PropertyFactory
 import org.maplibre.android.testapp.R
+import org.maplibre.android.testapp.activity.events.ObserverActivity.Companion.TAG
 import org.maplibre.android.testapp.styles.TestStyles
 import timber.log.Timber
 import java.util.*
+import kotlin.concurrent.fixedRateTimer
 
 /**
  * Test activity showcasing the different debug modes and allows to cycle between the default map styles.
@@ -32,6 +35,9 @@ open class DebugModeActivity : AppCompatActivity(), OnMapReadyCallback, OnFpsCha
     private var isReportFps = true
     private var isContinuousRendering = false
     private var fpsView: TextView? = null
+
+    private val renderingMetricTracker = RenderingMetricTracker()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_debug_mode)
@@ -39,6 +45,14 @@ open class DebugModeActivity : AppCompatActivity(), OnMapReadyCallback, OnFpsCha
         setupMapView(savedInstanceState)
         setupDebugChangeView()
         setupStyleChangeView()
+
+        renderingMetricTracker.setReportListener { encodingTime, numDrawCalls, memBuffers ->
+            Logger.i(TAG, "RenderingMetric encodingTime avg: ${encodingTime.getAvg() * 1000} | max: ${encodingTime.getMax() * 1000} | min: ${encodingTime.getMin() * 1000}")
+            Logger.i(TAG, "RenderingMetric numDrawCalls avg: ${numDrawCalls.getAvg().toInt()} | max: ${numDrawCalls.getMax().toInt()} | min: ${numDrawCalls.getMin().toInt()}")
+            Logger.i(TAG, "RenderingMetric memBuffers avg: ${memBuffers.getAvg().toInt()} | max: ${memBuffers.getMax().toInt()} | min: ${memBuffers.getMin().toInt()}")
+        }
+
+        renderingMetricTracker.startReports(10)
     }
 
     private fun setupToolbar() {
@@ -71,6 +85,10 @@ open class DebugModeActivity : AppCompatActivity(), OnMapReadyCallback, OnFpsCha
         mapView.onCreate(savedInstanceState)
         mapView.getMapAsync(this)
         mapView.addOnDidFinishLoadingStyleListener { Timber.d("Style loaded") }
+
+        mapView.addOnDidFinishRenderingFrameListener { _, stats->
+            renderingMetricTracker.addFrame(stats)
+        }
     }
 
     protected open fun setupMapLibreMapOptions(): MapLibreMapOptions {
@@ -218,6 +236,7 @@ open class DebugModeActivity : AppCompatActivity(), OnMapReadyCallback, OnFpsCha
             maplibreMap.removeOnCameraMoveListener(cameraMoveListener!!)
         }
         mapView.onDestroy()
+        renderingMetricTracker.stopReports()
     }
 
     override fun onLowMemory() {
@@ -275,5 +294,100 @@ open class DebugModeActivity : AppCompatActivity(), OnMapReadyCallback, OnFpsCha
             TestStyles.getPredefinedStyleWithFallback("Satellite Hybrid"),
             TestStyles.getPredefinedStyleWithFallback("Satellite Hybrid")
         )
+    }
+}
+
+class RenderingMetric {
+    private var min: Double = Int.MAX_VALUE.toDouble()
+    private var max: Double = Int.MIN_VALUE.toDouble()
+    private var total: Double = 0.0
+    private var frameCount = 0
+
+    fun getMin(): Double {
+        if (frameCount == 0)
+            return 0.0
+        return min
+    }
+
+    fun getMax(): Double {
+        if (frameCount == 0)
+            return 0.0
+        return max
+    }
+
+    fun getAvg(): Double {
+        if (frameCount == 0)
+            return 0.0
+        return total / frameCount
+    }
+
+    @Synchronized fun addFrame(value: Double) {
+        if (value < min) {
+            min = value
+        }
+        if (value > max) {
+            max = value
+        }
+        total = total + value
+        frameCount++;
+    }
+}
+
+class RenderingMetricTracker {
+    private var encodingTime = RenderingMetric()
+    private var numDrawCalls = RenderingMetric()
+    private var memBuffers = RenderingMetric()
+
+    private var intervalEncodingTime = RenderingMetric()
+    private var intervalNumDrawCalls = RenderingMetric()
+    private var intervalMemBuffers = RenderingMetric()
+
+    private var reportTimer: Timer? = null
+    private var reportListener: ((RenderingMetric, RenderingMetric, RenderingMetric) -> Unit)? = null
+
+    @Synchronized fun setReportListener(listener: ((RenderingMetric, RenderingMetric, RenderingMetric) -> Unit)?) {
+        reportListener = listener
+    }
+
+    @Synchronized fun startReports(interval: Long) {
+        reset()
+
+        reportTimer = fixedRateTimer(
+            name = "RenderingMetricReportTimer",
+            initialDelay = interval * 1000L,
+            period = interval * 1000L
+        ) {
+            reportListener?.invoke(intervalEncodingTime, intervalNumDrawCalls, intervalMemBuffers)
+            resetInterval()
+        }
+    }
+
+    @Synchronized fun stopReports() {
+        reportListener?.invoke(encodingTime, numDrawCalls, memBuffers)
+        reportTimer?.cancel()
+        reportTimer = null
+    }
+
+    @Synchronized fun addFrame(frameStats: RenderingStats) {
+        encodingTime.addFrame(frameStats.encodingTime)
+        numDrawCalls.addFrame(frameStats.numDrawCalls.toDouble())
+        memBuffers.addFrame(frameStats.memBuffers.toDouble())
+
+        intervalEncodingTime.addFrame(frameStats.encodingTime)
+        intervalNumDrawCalls.addFrame(frameStats.numDrawCalls.toDouble())
+        intervalMemBuffers.addFrame(frameStats.memBuffers.toDouble())
+    }
+
+    private fun reset() {
+        encodingTime = RenderingMetric()
+        numDrawCalls = RenderingMetric()
+        memBuffers = RenderingMetric()
+        resetInterval()
+    }
+
+    private fun resetInterval() {
+        intervalEncodingTime = RenderingMetric()
+        intervalNumDrawCalls = RenderingMetric()
+        intervalMemBuffers = RenderingMetric()
     }
 }
